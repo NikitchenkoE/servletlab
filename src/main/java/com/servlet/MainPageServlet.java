64e0604dbc9c9a206996e735e4dbef9f5e5f8fff
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-11-23 16:09:55 +0200

    fixed main servlet

--- src/main/java/com/servlet/MainPageServlet.java
+++ src/main/java/com/servlet/MainPageServlet.java
@@ -26,41 +26,57 @@ public class MainPageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         Map<String, Object> data = new HashMap<>();
-
+        data.put("products", new ArrayList<Product>());
         String description = req.getParameter("productDescription");
 
         if (req.getParameter("productDescription") == null && req.getParameter("productId") == null) {
             var products = productService.getAll();
-            var productsDto = mapToProductDtoList(products);
-            data.put("products", productsDto);
+            var productDtoList = mapToProductDtoList(products);
+            data.put("products", productDtoList);
+            resp.setStatus(HttpServletResponse.SC_OK);
         } else if (req.getParameter("productId") != null) {
-            List<Product> productList = new ArrayList<>();
-            long id = Long.parseLong(req.getParameter("productId"));
-            var product = productService.get(id).orElseThrow(() -> new RuntimeException("Cant be null"));
-            productList.add(product);
-            var productsDto = mapToProductDtoList(productList);
-            data.put("products", productsDto);
-        } else if (description != null) {
-            var products = productService.getByDescription(description);
-            var productsDto = mapToProductDtoList(products);
-            data.put("products", productsDto);
+            if (!req.getParameter("productId").isEmpty()) {
+                long id = Long.parseLong(req.getParameter("productId"));
+                data = getDataById(id);
+                resp.setStatus(HttpServletResponse.SC_OK);
+            }
+        } else if (description != null && !description.isEmpty()) {
+            data = getDataByDescription(description);
         } else {
-            resp.getWriter().println(PageGenerator.init().getPage("mainPage.ftlh"));
-            resp.setContentType("text/html;charset=utf-8");
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         }
-
         resp.getWriter().println(PageGenerator.init().getPage("mainPage.ftlh", data));
         resp.setContentType("text/html;charset=utf-8");
-        resp.setStatus(HttpServletResponse.SC_OK);
     }
-
 
     private List<ProductDto> mapToProductDtoList(List<Product> productList) {
         MapToProductDto mapToProductDto = new MapToProductDto();
         return productList.stream()
                 .map(mapToProductDto::mapToDto)
                 .collect(Collectors.toList());
+    }
+
+    private Map<String, Object> getDataById(long id) {
+        Map<String, Object> data = new HashMap<>();
+        List<Product> productList = new ArrayList<>();
+        List<ProductDto> productDtoList = new ArrayList<>();
+        var product = productService.get(id).orElse(null);
+        if (product != null) {
+            productList.add(product);
+            productDtoList = mapToProductDtoList(productList);
+            data.put("products", productDtoList);
+        } else {
+            data.put("products", productDtoList);
+        }
+        return data;
+    }
+
+    private Map<String, Object> getDataByDescription(String description) {
+        Map<String, Object> data = new HashMap<>();
+        var products = productService.getByDescription(description);
+        var productsDto = mapToProductDtoList(products);
+        data.put("products", productsDto);
+        return data;
     }
 
 
