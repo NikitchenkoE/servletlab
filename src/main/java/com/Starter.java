7b75fd4046f7ae96f9c7daaa46a27c91b0ec60e9
NikitchenkoE <66593735+NikitchenkoE@users.noreply.github.com>
2021-11-23 10:35:54 +0200

    cleaned code

--- src/main/java/com/Starter.java
+++ src/main/java/com/Starter.java
@@ -11,27 +11,22 @@ import org.eclipse.jetty.servlet.ServletHolder;
 @Slf4j
 public class Starter {
     public static void main(String[] args) throws Exception {
-        int port = 8080;
-        var portString = System.getenv("PORT");
         DataSourceFactory dataSourceFactory = new DataSourceFactory();
         ProductService productService = new ProductService(dataSourceFactory);
 
-        MainPageServlet mainPageServlet = new MainPageServlet(productService);
-        AddProductPageServlet addProductPageServlet = new AddProductPageServlet(productService);
-        UpdateProductServlet updateProductServlet = new UpdateProductServlet(productService);
-        DeleteServlet deleteServlet = new DeleteServlet(productService);
-
         ServletContextHandler servletContextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        servletContextHandler.addServlet(new ServletHolder(mainPageServlet), "/products");
-        servletContextHandler.addServlet(new ServletHolder(mainPageServlet), "/");
-        servletContextHandler.addServlet(new ServletHolder(addProductPageServlet), "/products/add");
-        servletContextHandler.addServlet(new ServletHolder(updateProductServlet), "/products/update");
-        servletContextHandler.addServlet(new ServletHolder(deleteServlet), "/products/delete");
+        servletContextHandler.addServlet(new ServletHolder(new MainPageServlet(productService)), "/products");
+        servletContextHandler.addServlet(new ServletHolder(new MainPageServlet(productService)), "/");
+        servletContextHandler.addServlet(new ServletHolder(new AddProductPageServlet(productService)), "/products/add");
+        servletContextHandler.addServlet(new ServletHolder(new UpdateProductServlet(productService)), "/products/update");
+        servletContextHandler.addServlet(new ServletHolder(new UpdateProductServlet(productService)), "/products/delete");
 
+        int port = 8080;
+        var portString = System.getenv("PORT");
         try {
             port = Integer.parseInt(portString);
         } catch (NumberFormatException e) {
-            log.error("Can't see port from env");
+            log.error("Can't see port from env. Will use {}",port);
         }
 
         Server server = new Server(port);
--- src/test/java/com/dao/SqlQueries.java
+++ src/test/java/com/dao/SqlQueries.java
@@ -1,6 +1,5 @@
 package com.dao;
 
 public interface SqlQueries {
-    String CREATE_TABLE_PRODUCTS = "CREATE TABLE products (productID IDENTITY NOT NULL PRIMARY KEY, name varchar, price double precision, description varchar, createDate timestamp, updateData timestamp)";
     String DROP_TABLE_PRODUCTS = "DROP TABLE products";
 }
